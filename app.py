cae737fac67bd2f76d5605edc5709c43ad2738c6
clairebogdan <bogdanclaire@gmail.com>
2020-09-03 15:10:13 -0400

    added footer

--- app.py
+++ app.py
@@ -113,10 +113,5 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/results", methods=["POST"])
-def results():
-    return render_template('results.html')
-
-
 if __name__ == "__main__":
     app.run(debug=True)
